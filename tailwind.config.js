6ecf602e278194ffff73b5221cd2454541f1b1e1
mrmn <mrmn@noreply.codeberg.org>
2024-07-23 21:43:22 +0200

    add colored themes to pdf and user html files
    mrmn/PdfDing#29

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   darkMode: 'selector',
   content: ["**/pdfding/**/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary1: "var(--color-tertiary1)",
+        tertiary2: "var(--color-tertiary2)",
+      },
+    },
   },
   plugins: [],
 }
