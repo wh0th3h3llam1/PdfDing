8f02388a1a875f9c46afd07774aaa558b51e1bc4
mrmn <mrmn@noreply.codeberg.org>
2024-10-26 22:04:43 +0200

    add test for default theme

--- pdfding/users/tests/test_models.py
+++ pdfding/users/tests/test_models.py
@@ -3,6 +3,14 @@ from django.test import TestCase
 
 
 class TestProfile(TestCase):
+    # override_settings is not working with this test as the models default value is not overwritten
+    # therefore do not change the theme defined in dev.py
+    def test_default_theme(self):
+        user = User.objects.create_user(username='user', password='12345', email='a@a.com')
+
+        self.assertEqual(user.profile.dark_mode, 'Dark')
+        self.assertEqual(user.profile.theme_color, 'Red')
+
     def test_dark_mode_str(self):
         user = User.objects.create_user(username='user', password='12345', email='a@a.com')
         user.profile.dark_mode = 'Dark'
