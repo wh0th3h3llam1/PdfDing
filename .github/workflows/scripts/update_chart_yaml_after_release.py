7387abbc5bd5ac3dde795d80e5fc3f282257873c
mrmn <188248364+mrmn2@users.noreply.github.com>
2025-02-03 18:35:24 +0100

    feat: do no update Chart.yaml if appVersion will not change

--- .github/workflows/scripts/update_chart_yaml_after_release.py
+++ .github/workflows/scripts/update_chart_yaml_after_release.py
@@ -12,23 +12,29 @@ def update_versions_after_release(chart_yaml_path: Path, release_tag: str) -> st
     be incremented by 1, the app version will be replaced by the 'release_tag'.
     """
 
-    with open(chart_yaml_path, "r") as f:
-        lines = f.readlines()[:-1]
+    with open(chart_yaml_path, 'r') as f:
+        lines = f.readlines()
+
+    app_version_line = lines.pop()
+    chart_version_line = lines.pop()
+
+    # check if the appVersion needs to be updated
+    if f'appVersion: {release_tag}' in app_version_line:
+        raise ValueError('appVersion is equal to release tag')
 
     # update chart version
-    chart_version_line = lines.pop()
-    chart_version_line_split = chart_version_line.rsplit(".", maxsplit=1)
-    patch = chart_version_line.split(".")[-1]
+    chart_version_line_split = chart_version_line.rsplit('.', maxsplit=1)
+    patch = chart_version_line.split('.')[-1]
     updated_patch = int(patch) + 1
-    updated_chart_version_line = f"{chart_version_line_split[0]}.{updated_patch}\n"
+    updated_chart_version_line = f'{chart_version_line_split[0]}.{updated_patch}\n'
 
     # update appVersion
-    updated_app_version_line = f"appVersion: {release_tag}\n"
+    updated_app_version_line = f'appVersion: {release_tag}\n'
 
     # update lines
     lines.extend([updated_chart_version_line, updated_app_version_line])
 
-    return "".join(lines)
+    return ''.join(lines)
 
 
 def gh_update_chart_yaml(
@@ -59,14 +65,18 @@ def gh_update_chart_yaml(
 
 
 def main():
-    file_path = "helm-charts/pdfding/Chart.yaml"
+    file_path = 'helm-charts/pdfding/Chart.yaml'
     chart_yaml_path = Path(__file__).parents[3] / file_path
-    release_tag = environ.get("RELEASE_TAG")
-    github_token = environ.get("GITHUB_TOKEN")
+    release_tag = environ.get('RELEASE_TAG')
+    github_token = environ.get('GITHUB_TOKEN')
 
-    chart_yaml_content = update_versions_after_release(chart_yaml_path, release_tag)
-    gh_update_chart_yaml("mrmn2/pdfding", "master", file_path, chart_yaml_content, release_tag, github_token)
+    try:
+        chart_yaml_content = update_versions_after_release(chart_yaml_path, release_tag)
+        gh_update_chart_yaml('mrmn2/pdfding', 'master', file_path, chart_yaml_content, release_tag, github_token)
+    # appVersion does not need to be updated
+    except ValueError:
+        pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
