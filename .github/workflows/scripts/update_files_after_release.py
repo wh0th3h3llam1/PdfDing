d0f21ec2f9fbee4b1a2f6b7e0e6c7ea7784ab1bc
mrmn2 <188248364+mrmn2@users.noreply.github.com>
2025-11-23 11:15:16 +0100

    ci/cd: Update package.json and pyproject.toml version field after release

--- .github/workflows/scripts/update_files_after_release.py
+++ .github/workflows/scripts/update_files_after_release.py
@@ -1,13 +1,12 @@
-"""
-Module for updating the changelog after a release.
-"""
+"""Module for updating the changelog, pyproject.toml and package.json after a release."""
 
+import json
 from argparse import ArgumentParser
 from datetime import datetime
 from pathlib import Path
 
 
-def update_changelog(changelog_path: Path, release_tag: str, release_body: str):
+def update_changelog(changelog_path: Path, release_tag: str, release_body: str) -> None:
     """Update the changelog based on the release tag and the release_body."""
 
     with open(changelog_path, 'r') as f:
@@ -31,19 +30,52 @@ def update_changelog(changelog_path: Path, release_tag: str, release_body: str):
         f.writelines(lines)
 
 
-def main():
+def update_pyproject(pyproject_path: Path, release_tag: str) -> None:
+    """Update the pyproject.toml after a release."""
+
+    with open(pyproject_path, 'r') as f:
+        lines = f.readlines()
+
+    if 'version' in lines[3]:
+        release_tag = release_tag.replace('v', '')
+        lines[3] = f'version = "{release_tag}"\n'
+    else:
+        raise ValueError('Version not present in line 4!')
+
+    with open(pyproject_path, 'w') as f:
+        f.writelines(lines)
+
+
+def update_package_json(package_json_path: Path, release_tag: str) -> None:
+    """Update the package.json after a release."""
+
+    with open(package_json_path, 'r') as f:
+        data = json.load(f)
+
+    release_tag = release_tag.replace('v', '')
+    data['version'] = release_tag
+
+    with open(package_json_path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+def main() -> None:
     parser = ArgumentParser()
-    for argument in ['release_tag', 'release_body', 'changelog_path']:
+    for argument in ['release_tag', 'release_body']:
         parser.add_argument(f'--{argument}')
     args = parser.parse_args()
 
     root_path = Path(__file__).parents[3]
-    changelog_path = root_path / args.changelog_path
+    changelog_path = root_path / 'CHANGELOG.md'
+    pyproject_path = root_path / 'pyproject.toml'
+    package_json_path = root_path / 'package.json'
 
     release_tag = args.release_tag
     release_body = args.release_body
 
     update_changelog(changelog_path, release_tag, release_body)
+    update_pyproject(pyproject_path, release_tag)
+    update_package_json(package_json_path, release_tag)
 
 
 if __name__ == '__main__':
